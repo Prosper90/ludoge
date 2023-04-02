46b569a81d1537a298be81e0bde1678c53a2f0a2
Prosper <nkonyeprospero@gmail.com>
2023-04-02 14:19:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home.js';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#000] text-sm font-semibold">
+
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={ <Home /> } />
+      </Routes>
+
+      <Footer />
+
+
     </div>
   );
 }
