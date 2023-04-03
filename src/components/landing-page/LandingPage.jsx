4285d668b326106d5d3fc35d3fa2037c235d3ca2
Prosper <nkonyeprospero@gmail.com>
2023-04-03 21:49:05 +0100

    final push

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Home from './Home';
 import SectionFive from './SectionFive';
 import SectionFour from './SectionFour';
@@ -8,19 +8,44 @@ import SectionTwo from './SectionTwo';
 import Sponsors from './Sponsors';
 import Footer from './Footer';
 import Roadmap from './Roadmap';
+import MoonLoader from "react-spinners/MoonLoader";
+import Fade from 'react-reveal/Fade';
 
 function LandingPage() {
+  
+  const [pageLoading, setPageLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPageLoading(false);
+    }, 5000);
+  }, [pageLoading])
+  
+
   return (
     <div className='text-sm md:text-md'>
-      <Home/>
-      <SectionOne/>
-      <SectionTwo/>
-      <SectionThree/>
-      <SectionFour/>
-      <Roadmap />
-      <SectionFive/>
-      <Sponsors />
-      <Footer />
+      {
+        !pageLoading ?
+          <Fade left>
+              <Home/>
+              <SectionOne/>
+              <SectionTwo/>
+              <SectionThree/>
+              <SectionFour/>
+              <Roadmap />
+              <SectionFive/>
+              <Sponsors />
+              <Footer />
+          </Fade>
+        :
+      <div className='absolute flex justify-center items-center p-4 w-[100%] h-[100%] bg-[#0C0F16]' >
+        <MoonLoader
+             loading={pageLoading}
+             color={`#734D08`}
+             size={70}
+        />
+     </div>
+      }
     </div>
   )
 }
--- src/components/landing-page/Sponsors.js
+++ src/components/landing-page/Sponsors.js
@@ -6,39 +6,39 @@ export default function Sponsors() {
       <div className="w-100 h-[200px] flex flex-col gap-10">
 
         <h1 className='md:text-5xl pt-2 px-5 text-[#734D08] text-3xl font-bold text-center md:text-start'>
-					Sponsors
+					Partners
 				</h1>
         
         <div className="flex gap-[4.75rem] pt-5 px-7 w-full md:w-full  flex-nowrap whitespace-nowrap overflow-hidden overflow-x-scroll scrollbar-hide items-center h-auto scrollbar-hide">
 
-          <a href='#' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
+          <a href='https://www.pinksale.finance' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
             <img src="/sponsorone.png" alt="sponsor" className='w-[100%] md:w-[50%] h-auto' />
-            <span className='font-thin text-sm'>sponsor one</span>
+            <span className='font-thin text-sm'>Pinksale</span>
           </a>
 
-          <a href='#' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
+          <a href='https://www.dexview.com' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
             <img src="/sponsortwo.png" alt="sponsor" className='w-[100%] md:w-[50%] h-auto' />
-            <span className='font-thin text-sm'>sponsor one</span>
+            <span className='font-thin text-sm'>Dex view</span>
           </a>
 
-          <a href='#' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
+          <a href='https://bscscan.com' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
             <img src="/sponsorthree.png" alt="sponsor" className='w-[100%] md:w-[50%] h-auto' />
-            <span className='font-thin text-sm'>sponsor one</span>
+            <span className='font-thin text-sm'>Bsc</span>
           </a>
 
-          <a href='#' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
+          <a href='https://pancakeswap.finance' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
             <img src="/sponsorfour.png" alt="sponsor" className='w-[100%] md:w-[50%] h-auto' />
-            <span className='font-thin text-sm'>sponsor one</span>
+            <span className='font-thin text-sm'>pancakeswap</span>
           </a>
 
-          <a href='#' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
+          <a href='https://poocoin.app' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
             <img src="/sponsorfive.png" alt="sponsor" className='w-[100%] md:w-[50%] h-auto' />
-            <span className='font-thin text-sm'>sponsor one</span>
+            <span className='font-thin text-sm'>poocoin</span>
           </a>
 
-          <a href='#' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
+          <a href='https://www.dextools.io' className='w-[10%] h-auto flex flex-col justify-center items-center' > 
             <img src="/sponsorsix.png" alt="sponsor"  className='w-[100%] md:w-[50%] h-auto' />
-            <span className='font-thin text-sm'>sponsor one</span>
+            <span className='font-thin text-sm'>Dex tools</span>
           </a>
 
         </div>    
